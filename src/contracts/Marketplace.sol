bdc75471e55458dd749e4f74fa1581a375d3519b
draichi <lucasdraichi@gmail.com>
2019-10-27 19:53:08 -0300

    addind tests

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -1,9 +1,11 @@
 pragma solidity ^0.5.0;
 
 contract Marketplace {
-    string name;
+    string public name;
+    string public jurere;
 
     constructor() public {
         name = "Market Place hello world like";
+        jurere = "oi";
     }
 }
